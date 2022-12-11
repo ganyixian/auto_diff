720f8f0b1ad585a7b8ca99ffd789a8a8fdde683d
Phoebe <tingcheng@g.harvard.edu>
2022-12-11 18:03:05 -0500

    finish the simple optimization program

--- tests/optimization/optimization_demo.py
+++ tests/optimization/optimization_demo.py
@@ -1,18 +1,25 @@
-# Performing gradient descent 
+import sys
+sys.path.append('src/')
+sys.path.append('../../src')
 
-from auto_diff.expression import Expression, Variable, Function, ops, Compose
+from auto_diff_CGLLY.expression import Variable
 import numpy as np
 
-x = 3
-step_scale = 0.3
-num_steps = 20
-print(x)
+''' A simple program to find the mininum of f = x^2'''
 
-for step_i in range(num_steps):
-    x = Variable('x')
-    x, y = Variable.vars(['x', 'y'], 'r')
-    f = Compose([x ** 2 + x * y, x-y])
-    inputs = {'x': np.array([1, 2]), 'y': 2}
-    res = f(inputs)
-    x = x - step_scale * res[1] 
-    print(x)
+x = Variable('x', mode = 'r')
+f = x ** 2
+learning_rate = 0.2
+tol = 1e-12
+iter_count = 500
+initial_guess = 10
+position = initial_guess
+for i in range(iter_count):
+    f_val, grad = f({'x': float(position)}) 
+    grad = grad.get('x')
+    diff = -learning_rate * grad
+    if np.abs(diff) <= tol:
+        break
+    position += diff
+print("The minimum for f = x^2 is", position)
+ 
